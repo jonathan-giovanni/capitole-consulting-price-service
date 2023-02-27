e547949f8d65aed61359edd0d39f63010144a02a
jonathan-giovanni <hv12i04001@gmail.com>
2023-02-27 17:09:14 +0100

    Add @Schema to every field in the ErrorResponse

--- src/main/java/com/capitoleconsulting/price/service/infrastructure/web/controller/ErrorResponse.java
+++ src/main/java/com/capitoleconsulting/price/service/infrastructure/web/controller/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.capitoleconsulting.price.service.infrastructure.web.controller;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 
@@ -10,10 +11,15 @@ import java.time.LocalDateTime;
  * @created 27/02/2023
  */
 public record ErrorResponse(
+        @Schema(description = "Error timestamp",example = "2023-02-26T20:53:04.663Z")
         @JsonProperty LocalDateTime timestamp,
+        @Schema(description = "Error status code",example = "404")
         @JsonProperty Integer status,
+        @Schema(description = "Error title",example = "Price not found")
         @JsonProperty String error,
+        @Schema(description = "Error message",example = "Price not found with the given id")
         @JsonProperty String message,
+        @Schema(description = "Requested path",example = "/v1/price/")
         @JsonProperty String path
 ) {
     public ErrorResponse(LocalDateTime timestamp, Integer status, String error, String message, String path) {

0198a23a864fcc8a0bf005911257395e2d617b50
jonathan-giovanni <hv12i04001@gmail.com>
2023-02-27 17:19:01 +0100

    Add documentation to GetMethod with possibles responses

--- src/main/java/com/capitoleconsulting/price/service/infrastructure/web/controller/PriceController.java
+++ src/main/java/com/capitoleconsulting/price/service/infrastructure/web/controller/PriceController.java
@@ -1,6 +1,10 @@
 package com.capitoleconsulting.price.service.infrastructure.web.controller;
 
 import com.capitoleconsulting.price.service.application.search.SearchPrice;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +29,10 @@ public class PriceController {
         this.searchPrice = searchPrice;
     }
 
+    @Operation(summary="Get price of product in specified date",description="Get price of product by productId, brandId and applicationDate in range of prices")
+    @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PriceResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Price Not Found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @GetMapping("/product/{productId}/brand/{brandId}/date/{applicationDate}")
     public ResponseEntity<PriceResponse> findProductPrice(@PathVariable Long productId, @PathVariable Long brandId, @PathVariable LocalDateTime applicationDate) {
         var data = searchPrice.execute(applicationDate,productId,brandId);

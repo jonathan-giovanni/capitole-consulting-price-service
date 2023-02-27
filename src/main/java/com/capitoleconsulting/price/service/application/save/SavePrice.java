79dd87954fb2e5a154378f3ba8b057ae3d3c6f83
jonathan <hv12i04001@gmail.com>
2023-02-27 08:53:07 +0100

    Add SavePrice that execute: loadData and saveAll loaded data

---
+++ src/main/java/com/capitoleconsulting/price/service/application/save/SavePrice.java
@@ -0,0 +1,25 @@
+package com.capitoleconsulting.price.service.application.save;
+
+import com.capitoleconsulting.price.service.domain.port.LoadPriceRepository;
+import com.capitoleconsulting.price.service.domain.port.SavePriceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SavePrice {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final LoadPriceRepository loadPriceRepository;
+    private final SavePriceRepository savePriceRepository;
+
+    public SavePrice(LoadPriceRepository loadPriceRepository, SavePriceRepository savePriceRepository) {
+        this.loadPriceRepository = loadPriceRepository;
+        this.savePriceRepository = savePriceRepository;
+    }
+
+    public void execute(){
+        var loadedData = loadPriceRepository.loadPrices();
+        log.info("Loaded data: {}",loadedData.count());
+        savePriceRepository.saveAll(loadedData);
+    }
+
+
+}

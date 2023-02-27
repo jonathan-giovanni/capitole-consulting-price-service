44d7a4df7049d869dd1755897ca8902915bd8a3b
jonathan <hv12i04001@gmail.com>
2023-02-27 07:17:01 +0100

    Initial project

---
+++ src/main/java/com/capitoleconsulting/price/service/CapitoleConsultingPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capitoleconsulting.price.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleConsultingPriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitoleConsultingPriceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitoleconsulting/price/service/CapitoleConsultingPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitoleconsulting.price.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitoleConsultingPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

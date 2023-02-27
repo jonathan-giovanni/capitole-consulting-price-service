fd059083d4f050c0cf3a91951689572f8f5c2a90
jonathan-giovanni <hv12i04001@gmail.com>
2023-02-27 14:16:08 +0100

    Add method to findAllByApplicationDateBetweenAndProductIdAndBrandId

--- src/main/java/com/capitoleconsulting/price/service/infrastructure/adapter/repository/jpa/JpaPriceRepository.java
+++ src/main/java/com/capitoleconsulting/price/service/infrastructure/adapter/repository/jpa/JpaPriceRepository.java
@@ -1,9 +1,15 @@
 package com.capitoleconsulting.price.service.infrastructure.adapter.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 @Repository
 public interface JpaPriceRepository extends JpaRepository<JpaPriceEntity,JpaPriceEntityPk> {
-
+    @Query("SELECT p FROM JpaPriceEntity p WHERE :applicationDate >= p.startDate AND :applicationDate <= p.endDate AND p.pk.productId = :productId AND p.pk.brandId = :brandId ")
+    Stream<JpaPriceEntity> findAllByApplicationDateBetweenAndProductIdAndBrandId(LocalDateTime applicationDate,Long productId,Long brandId);
 }

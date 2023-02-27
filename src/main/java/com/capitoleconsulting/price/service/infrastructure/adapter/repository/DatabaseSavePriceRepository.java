4c91769d3726cb349fdc94411885243fa3b2d672
jonathan-giovanni <hv12i04001@gmail.com>
2023-02-27 14:59:19 +0100

    Add jpaPriceRepository.saveAll implementation

--- src/main/java/com/capitoleconsulting/price/service/infrastructure/adapter/repository/DatabaseSavePriceRepository.java
+++ src/main/java/com/capitoleconsulting/price/service/infrastructure/adapter/repository/DatabaseSavePriceRepository.java
@@ -2,7 +2,11 @@ package com.capitoleconsulting.price.service.infrastructure.adapter.repository;
 
 import com.capitoleconsulting.price.service.domain.model.Price;
 import com.capitoleconsulting.price.service.domain.port.SavePriceRepository;
+import com.capitoleconsulting.price.service.infrastructure.adapter.repository.jpa.JpaPriceEntity;
+import com.capitoleconsulting.price.service.infrastructure.adapter.repository.jpa.JpaPriceRepository;
+import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -10,12 +14,27 @@ import java.util.stream.Stream;
  * @company Finansoportes Consulting
  * @created 27/02/2023
  */
+@Component
 public class DatabaseSavePriceRepository implements SavePriceRepository {
 
+    private final JpaPriceRepository jpaPriceRepository;
 
+    public DatabaseSavePriceRepository(JpaPriceRepository jpaPriceRepository) {
+        this.jpaPriceRepository = jpaPriceRepository;
+    }
 
     @Override
     public void saveAll(Stream<Price> prices) {
-
+        jpaPriceRepository.saveAll(prices.map(p->new JpaPriceEntity(
+                p.brandId(),
+                p.startDate(),
+                p.endDate(),
+                p.priceList(),
+                p.productId(),
+                p.priority(),
+                p.price(),
+                p.currency(),
+                p.updatedAt()
+        )).toList());
     }
 }

350bc44eb17096f7c109277878a48fc88b91ed8f
jonathan <hv12i04001@gmail.com>
2023-02-27 08:41:22 +0100

    Add test case searchPriceExpectedPriceNotFoundException

--- src/test/java/com/capitoleconsulting/price/service/application/PriceTestStub.java
+++ src/test/java/com/capitoleconsulting/price/service/application/PriceTestStub.java
@@ -11,6 +11,7 @@ public class PriceTestStub {
 
     public static final LocalDateTime applicationDate = LocalDateTime.parse("2020-06-14T10:00:00");
     public static final Long existingProductId = 35455L;
+    public static final Long nonExistProductId = 123L;
     public static final Long existingBrandId = 1L;
 
     public static final Price expectedPrice = new Price(1L,LocalDateTime.parse("2020-06-14T00:00:00"),LocalDateTime.parse("2020-12-31T23:59:59"),1L,35455L,0, BigDecimal.valueOf(35.50),"EUR",LocalDateTime.parse("2023-02-27T10:00:00"));
--- src/test/java/com/capitoleconsulting/price/service/application/search/SearchPriceTest.java
+++ src/test/java/com/capitoleconsulting/price/service/application/search/SearchPriceTest.java
@@ -1,6 +1,7 @@
 package com.capitoleconsulting.price.service.application.search;
 
 import com.capitoleconsulting.price.service.application.PriceTestStub;
+import com.capitoleconsulting.price.service.domain.exception.PriceNotFoundException;
 import com.capitoleconsulting.price.service.domain.port.SearchPriceRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +18,11 @@ public class SearchPriceTest {
     private SearchPrice searchPrice;
     private SearchPriceRepository searchPriceRepository;
 
+    private void mock_find_price_and_return_data_to_repository(){
+        log.info("Mock find price using -> applicationDate: {} , productId: {} , brandId: {}", PriceTestStub.applicationDate,PriceTestStub.existingProductId,PriceTestStub.existingBrandId);
+        Mockito.when(searchPriceRepository.findPriceInDateAndProductIdAndBrandId(PriceTestStub.applicationDate,PriceTestStub.existingProductId,PriceTestStub.existingBrandId)).thenReturn(PriceTestStub.expectedPrices);
+    }
+
     @BeforeEach
     void setupTest(){
         log.info("Setup test");
@@ -26,12 +32,22 @@ public class SearchPriceTest {
 
     @Test
     void searchPriceExpectedTheSamePrice() {
-
-        log.info("Mock find price using -> applicationDate: {} , productId: {} , brandId: {}", PriceTestStub.applicationDate,PriceTestStub.existingProductId,PriceTestStub.existingBrandId);
-        Mockito.when(searchPriceRepository.findPriceInDateAndProductIdAndBrandId(PriceTestStub.applicationDate,PriceTestStub.existingProductId,PriceTestStub.existingBrandId)).thenReturn(PriceTestStub.expectedPrices);
+        mock_find_price_and_return_data_to_repository();
 
         var priceFound = searchPrice.execute(PriceTestStub.applicationDate,PriceTestStub.existingProductId,PriceTestStub.existingBrandId);
         log.info("Expected the same price");
         Assertions.assertEquals(PriceTestStub.expectedPrice,priceFound);
     }
+
+    @Test
+    void searchPriceExpectedPriceNotFoundException() {
+        mock_find_price_and_return_data_to_repository();
+
+        Assertions.assertThrows(
+                PriceNotFoundException.class,
+                () -> searchPrice.execute(PriceTestStub.applicationDate,PriceTestStub.nonExistProductId,PriceTestStub.existingBrandId)
+        );
+    }
+
+
 }

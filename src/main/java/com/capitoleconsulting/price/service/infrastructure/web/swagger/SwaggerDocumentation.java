9daa278032beacd6a7aa44cfc9d3937c40ecdf24
jonathan-giovanni <hv12i04001@gmail.com>
2023-02-27 17:07:08 +0100

    Add SwaggerDocumentation

---
+++ src/main/java/com/capitoleconsulting/price/service/infrastructure/web/swagger/SwaggerDocumentation.java
@@ -0,0 +1,27 @@
+package com.capitoleconsulting.price.service.infrastructure.web.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+
+/**
+ * @author Jonathan Giovanni Hernandez
+ * @company Finansoportes Consulting
+ * @created 27/02/2023
+ */
+@Configuration
+public class SwaggerDocumentation {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("PRICE-SERVICE").description(
+                        "Price microservice API doc by OpenAPI 3.").version("1.0.0"));
+    }
+
+
+}
